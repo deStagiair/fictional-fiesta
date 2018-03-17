2ae1b04adf32c144e6f6ff0add0506d88f2f5bab
deStagiair <ikBenDeStagiair@gmail.com>
2018-03-17 13:48:34 +0100

    Add check target using cppcheck

--- site_cmake/FindPugiXML.cmake
+++
@@ -1,5 +0,0 @@
-find_path(PUGIXML_INCLUDE_DIR NAMES pugixml.hpp)
-find_library(PUGIXML_LIBRARIES NAMES pugixml)
-
-message (STATUS "Found pugixml includes: ${PUGIXML_INCLUDE_DIR}")
-message (STATUS "Found pugixml libraries: ${PUGIXML_LIBRARIES}")
